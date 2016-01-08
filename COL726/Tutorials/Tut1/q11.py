36a0b3bd87b392a39e3b8990981ca7a9335a9201
kartikeyagup <kartikeyagupta@hotmail.com>
2016-01-08 18:01:32 +0530

    q11 done

---
+++ COL726/Tutorials/Tut1/q11.py
@@ -0,0 +1,21 @@
+import math
+
+def Integrate(f, start,stop, step):
+    ans = 0.0
+    elem = start
+    while (elem<stop):
+        ans += 0.5*step*(f(elem)+f(elem+step))
+        elem += step
+    # for elem in xrange(start,stop,step):
+        # ans += 0.5*(f(elem)+f(elem+step))
+    return ans
+
+print "test"
+
+def fact(n):
+    return n*2.0
+print math.sin(math.pi/2)
+print Integrate(fact,1,2,0.5)
+print Integrate(math.sin,0,math.pi,0.1)
+print Integrate(math.sin,0,math.pi,0.01)
+print Integrate(math.sin,0,math.pi,0.001)
