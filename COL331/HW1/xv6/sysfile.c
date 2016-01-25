be893296e8f2ae0b24781b923361d4e4102cdba8
kartikeyagup <kartikeyagupta@hotmail.com>
2016-01-26 00:03:54 +0530

    Part 1 done

--- COL331/HW1/xv6/sysfile.c
+++ COL331/HW1/xv6/sysfile.c
@@ -54,13 +54,21 @@ sys_dup(void)
 {
   struct file *f;
   int fd;
-  
+
   if(argfd(0, 0, &f) < 0)
     return -1;
   if((fd=fdalloc(f)) < 0)
     return -1;
   filedup(f);
   return fd;
+}
+
+int sys_dup2(void)
+{
+    struct file *f;
+    int fd1;
+    int fd2;
+
 }
 
 int
@@ -92,7 +100,7 @@ sys_close(void)
 {
   int fd;
   struct file *f;
-  
+
   if(argfd(0, &fd, &f) < 0)
     return -1;
   proc->ofile[fd] = 0;
@@ -105,7 +113,7 @@ sys_fstat(void)
 {
   struct file *f;
   struct stat *st;
-  
+
   if(argfd(0, 0, &f) < 0 || argptr(1, (void*)&st, sizeof(*st)) < 0)
     return -1;
   return filestat(f, st);
@@ -353,7 +361,7 @@ sys_mknod(void)
   char *path;
   int len;
   int major, minor;
-  
+
   begin_op();
   if((len=argstr(0, &path)) < 0 ||
      argint(1, &major) < 0 ||
